1cee3d2b0eb97f4c8b43efaa258e8acc5252a816
Oriel10 <orielav10@gmail.com>
2021-12-11 14:34:31 +0200

    add implementation

---
+++ build/CMakeFiles/CalcMathExp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CalcMathExp.dir/main.cpp.obj"
+  "CalcMathExp.exe"
+  "CalcMathExp.exe.manifest"
+  "CalcMathExp.pdb"
+  "libCalcMathExp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CalcMathExp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/user/Desktop/git projects/Calculate-Math-Expression
+# Build directory: C:/Users/user/Desktop/git projects/Calculate-Math-Expression/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
